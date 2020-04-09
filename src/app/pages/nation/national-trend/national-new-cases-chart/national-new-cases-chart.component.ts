9d6072ecc8e2d67ad9b985fc2042260e6758aa04
andrea.como <andrea.como@gmail.com>
2020-04-09 23:25:15 +0200

    New cases with sma

--- src/app/pages/nation/national-trend/national-new-cases-chart/national-new-cases-chart.component.ts
+++ src/app/pages/nation/national-trend/national-new-cases-chart/national-new-cases-chart.component.ts
@@ -23,23 +23,44 @@ export class NationalNewCasesChartComponent implements OnInit, OnChanges {
 
   chartData: ChartDataSets[];
 
+  window: number;
+
   constructor(private dateString: DateStringPipe) { }
 
   ngOnInit() {
+    this.window = 5;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.data.currentValue && !changes.data.previousValue) {
-      const latestData = this.data.slice(this.data.length - 30);
-      let index = 1;
+      const latestData = this.data.slice(this.data.length - 40);
       this.chartData = [{
           label: 'Totale casi',
           data: latestData.map(d => d.nuovi_positivi),
-          // barPercentage: .4,
+          barPercentage: .6,
           backgroundColor: '#ff9800AA',
           hoverBackgroundColor: '#ff7b00'
+        }, {
+          label: `Media Mobile a ${window} giorni`,
+          data: this.createSimpleMovingAverage(latestData, this.window, 'nuovi_positivi'),
+          type: 'line',
+          fill: false
         }];
       this.labels = latestData.map(d => this.dateString.transform(d.data));
     }
+  }
+
+  private createSimpleMovingAverage(data: NationalData[], window: number, field: string): number[] {
+    const gap = Math.floor(window / 2);
+    const result: number[] = Array(gap).fill(null);
+
+    for (let i = 0; i < data.length - window; i++) {
+      const sma = data.slice(i, i + window).reduce((acc, d) => {
+        return acc + d[field];
+      }, 0) / window;
+      result.push(sma);
+    }
+
+    return result;
   }
 }
