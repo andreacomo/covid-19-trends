88d3dbaf2ba187e0d31c027e461618af283715e7
andrea.como <andrea.como@gmail.com>
2021-01-03 11:18:26 +0100

    Restyling

---
+++ src/app/commons/services/vaccination.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class VaccinationService {
+
+    static url = 'https://wabi-europe-north-b-api.analysis.windows.net/public/reports/querydata?synchronous=true';
+
+    
+}
