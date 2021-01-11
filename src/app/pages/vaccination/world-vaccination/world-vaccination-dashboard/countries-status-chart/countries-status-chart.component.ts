1f9c327a157a1527b7c0e617dff911700913423d
andrea.como <andrea.como@gmail.com>
2021-01-12 00:53:50 +0100

    Chart restyling

--- src/app/pages/vaccination/world-vaccination/world-vaccination-dashboard/countries-status-chart/countries-status-chart.component.ts
+++ src/app/pages/vaccination/world-vaccination/world-vaccination-dashboard/countries-status-chart/countries-status-chart.component.ts
@@ -3,6 +3,8 @@ import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Label } from 'ng2-charts';
 import { CountryVaccinationStatus } from '../../models/country-vaccination-status';
 import { WorldVaccinationStatus } from '../../models/world-vaccination-status';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { Colors } from 'src/app/commons/models/colors';
 
 @Component({
   selector: 'app-countries-status-chart',
@@ -22,25 +24,64 @@ export class CountriesStatusChartComponent implements OnInit, OnChanges {
 
   chartData: ChartDataSets[];
 
-  constructor() { }
+  constructor() {
+    this.plugins = [pluginDataLabels];
+    this.options = {
+      responsive: true,
+      aspectRatio: 2,
+      legend: {
+        display: true,
+        position: 'top',
+        align: 'center',
+        labels: {
+        boxWidth: 13,
+        fontFamily: 'Roboto, \'Helvetica Neue\', sans-serif'
+        }
+      },
+      tooltips: {
+          enabled: false
+      },
+      plugins: {
+        datalabels: {
+          anchor: 'end',
+          align: 'end',
+          formatter: (value, ctx) => {
+            return parseInt(value, 10).toLocaleString();
+          }
+        }
+      }
+    };
+   }
 
   ngOnInit(): void {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.data.currentValue && !changes.data.previousValue) {
-      const countryWithMaxTotalVaccinationsPerHundred = this.data
-        .map(country => country.data.reduce(this.maxTotalVaccinationsPerHundred))
-        .sort((c1, c2) => c2.totalVaccinationsPerHundred - c1.totalVaccinationsPerHundred)
+      const field = 'totalVaccinations';
+      const countryWithMaxFieldValue = this.data
+        .map(country => country.data.reduce((prev, curr) => this.max(field, prev, curr)))
+        .sort((c1, c2) => this.sort(field, c1, c2))
         .slice(0, 20);
-      this.labels = countryWithMaxTotalVaccinationsPerHundred.map(c => c.countryName);
+      this.labels = countryWithMaxFieldValue.map(c => c.countryName);
       this.chartData = [{
-        data: countryWithMaxTotalVaccinationsPerHundred.map(c => c.totalVaccinationsPerHundred)
+        data: countryWithMaxFieldValue.map(c => this.getFieldValue(field, c)),
+        backgroundColor: Colors.SUPPORTED,
+        borderColor: Colors.SUPPORTED,
+        hoverBackgroundColor: Colors.SUPPORTED.map(c => c + 'DD')
       }];
     }
   }
 
-  private maxTotalVaccinationsPerHundred(prev: CountryVaccinationStatus, current: CountryVaccinationStatus): CountryVaccinationStatus {
-    return prev.totalVaccinationsPerHundred > current.totalVaccinationsPerHundred ? prev : current;
+  private max(fieldName: string, prev: CountryVaccinationStatus, current: CountryVaccinationStatus): CountryVaccinationStatus {
+    return prev[fieldName] > current[fieldName] ? prev : current;
+  }
+
+  private sort(fieldName: string, c1: CountryVaccinationStatus, c2: CountryVaccinationStatus): number {
+    return c2[fieldName] - c1[fieldName];
+  }
+
+  private getFieldValue(fieldName: string, c: CountryVaccinationStatus): number {
+    return c[fieldName];
   }
 }
